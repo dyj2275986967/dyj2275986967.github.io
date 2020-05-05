644f2c15991793d6cef93acb4391c9fb94254881
alublabla <1009730494@qq.com>
2020-05-05 19:03:34 +0800

    Site updated: 2020-05-05 19:03:22

---
+++ dist/music.js
@@ -0,0 +1,58 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "人间",
+        artist: '王菲',
+        url: 'http://m8.music.126.net/20200505163955/9f618fc7a8cbde29287943a006ba3a44/ymusic/9399/08cd/a6f4/3248954dbb7caf8738f1d929a37c63d9.mp3',
+        cover: 'https://p2.music.126.net/6DS0esuwYLLgf9VyYi4R1A==/109951163165829326.jpg',
+      },
+      {
+        name: '你是人间四月天',
+        artist: '解忧邵帅',
+        url: 'http://m8.music.126.net/20200505164418/b702ca0f32fa20e2ae54d66b1491e7e5/ymusic/5152/5552/030c/f860fa842eb8201274264f2d9eda6139.mp3',
+        cover: 'https://p2.music.126.net/kQJQD58WkmQneTneOYe22A==/109951164252455813.jpg',
+      },
+      {
+        name: '胡广生',
+        artist: '任素汐',
+        url: 'http://m8.music.126.net/20200505165008/369f5fe7b311ae9ebf093778cfb67511/ymusic/12e7/c06a/8363/96f7458345b2962b3309970ca4db0d85.mp3',
+        cover: 'https://p2.music.126.net/fG0T1hnM9EEmkYSo4Ey97A==/109951163669946483.jpg',
+      },
+	  {
+        name: '如果有来生',
+        artist: '谭维维',
+        url: 'http://m8.music.126.net/20200505165335/f8abcbe73271e89ed96da52c793e158b/ymusic/6056/df5d/53ae/e1fdd1a198d0f2b2523f5760d7c03411.mp3',
+        cover: 'https://p2.music.126.net/V1oiaFl104PH0yuHLCKVuQ==/109951163061827624.jpg',
+      },
+	  {
+        name: '父亲写的散文诗',
+        artist: '许飞',
+        url: 'http://m8.music.126.net/20200505165527/aa2fe3e13a5bcce03d94685857cf7ff6/ymusic/fb3d/92d0/611a/22abff38fbdb93fea0ef44274038b349.mp3',
+        cover: 'https://p2.music.126.net/klZM86P-K_DHWpOj0gJ3Mg==/3252355400229043.jpg',
+      }
+	  ,
+	  {
+        name: '只要平凡',
+        artist: '小石头和孩子们',
+        url: 'http://m8.music.126.net/20200505165757/bdde3945aba817e772c5698ff1fe46b1/ymusic/6106/7558/197c/eede38e57f4a0e2983b73f742b2e8c79.mp3',
+        cover: 'https://p1.music.126.net/Ra3sZxrKPDqT7DM_ZFlQmA==/109951163693332291.jpg',
+      }
+	   ,
+	  {
+        name: '这城市风很大',
+        artist: '枯木逢春',
+        url: 'http://m7.music.126.net/20200505170209/8b1b7e5eda3560d381918adfb6a41427/ymusic/0e5e/555e/5252/94548785c7b40c99bd67e8aefa6ec37a.mp3',
+        cover: 'https://p2.music.126.net/415A5Xt3bUegx4vyiY8dNQ==/109951164821961505.jpg',
+      }
+	     ,
+	  {
+        name: '不下雪的广东',
+        artist: '广东雨神',
+        url: 'http://m8.music.126.net/20200505170503/9ec80cddc5563836c3021f11afa39305/ymusic/535c/545e/5359/d686b2d830c477bce51ad35668166d60.mp3',
+        cover: 'https://p2.music.126.net/adZY9BenYJC8qvUAufA4jg==/109951163903661229.jpg',
+      }
+    ]
+});
